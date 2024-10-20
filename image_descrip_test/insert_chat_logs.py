fe437b01fd767de6522da53d5ffe965bb370e622
Jonathan Kim <jonathankim@Jonathans-MacBook-Air-3.local>
2024-10-19 17:04:21 -0700

    ignore

--- image_descrip_test/insert_chat_logs.py
+++ image_descrip_test/insert_chat_logs.py
@@ -1,6 +1,9 @@
 from singlestore import insert_chat_log
 from singlestore import steps
 from deepgram_api import main
+# Take input from Flask from Unity of transcripts
+# Run function to import text into Unity with correct Speaker
+# Audio to text ocming in is always user 
 # from __file__ import __check_if_step_is_complete__
 
 step_counter = 1
