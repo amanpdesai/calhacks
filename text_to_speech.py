f0134313a6a4428a9d043d105606d0abdb9d13eb
Allison Jiang <allison.x.jiang@gmail.com>
2024-10-19 21:59:31 -0700

    Created TTS file.

---
+++ text_to_speech.py
@@ -0,0 +1,33 @@
+import http.client
+import json
+import os
+
+
+def text_to_wav(given_text, output_dir="../../Assets/TextToSpeech/"):
+    url = "api.deepgram.com"
+    conn = http.client.HTTPSConnection(url)
+    headers = {
+        "Authorization": "Token 077b7002b19330ce99f96adf7134bf8c50896f78",
+        "Content-Type": "application/json"
+    }
+
+    request_body = json.dumps({"text": given_text})
+    conn.request(
+        "POST", "/v1/speak?model=aura-luna-en&encoding=linear16", request_body, headers)
+    response = conn.getresponse()
+
+    # Count all files in the directory (assuming only .wav files exist)
+    i = len(os.listdir(output_dir)) + 1
+
+    # Create a new file path using the count
+    file_path = f"tts_file_{i}.wav"
+
+    with open(os.path.join(output_dir, file_path), "wb") as output_file:
+        output_file.write(response.read())
+
+    conn.close()
+    print(f"File saved as {file_path}")
+
+
+if __name__ == "__main__":
+    text_to_wav("Hello, I am a demo of the text to speech service.", output_dir="./")
